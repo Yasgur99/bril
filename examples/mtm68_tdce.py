a506ba92717c96a1b2f9aa77a97344102d103ab0
Michael Maitland <mtm68@cornell.edu>
2022-02-10 21:40:25 -0500

    fix bugs

--- examples/mtm68_tdce.py
+++ examples/mtm68_tdce.py
@@ -5,18 +5,16 @@ from util import flatten
 
 def fix_map(last_def, idx):
     """
-    Decreases all values by 1 that are greater or equal to idx in last_def.
+    Decreases all values by 1 that are greater to idx in last_def.
     """
     for k in last_def.keys():
-        if last_def[k] >= idx:
+        if last_def[k] > idx:
             last_def[k] = last_def[k] - 1
 
-def use_defs(block):
-    # var -> instrId; variables defined by inst but not yet used
-    last_def = {} 
+def removed_overwritten_before_use(block):
+    last_def = {} # var -> instrIdx; 
     deleted_inst = False
-
-    j = 0
+    j = 0 # used to keep index in bounds because deleting during iterating
     for i in range(len(block)):
         instr = block[i-j]
         # check for use
@@ -31,13 +29,14 @@ def use_defs(block):
                 idx = last_def[instr['dest']]
                 block.pop(idx)
                 fix_map(last_def, idx)
-                last_def[instr['dest']] = i
+                last_def[instr['dest']] = i - 1
                 deleted_inst = True
                 j+=1
             else:
                 last_def[instr['dest']] = i
 
     return deleted_inst 
+
 
 def remove_unused(instrs):
     """
@@ -48,7 +47,8 @@ def remove_unused(instrs):
     for instr in instrs:
         if 'args' in instr:
             for arg in instr['args']:
-                defined_but_not_used.remove(arg)
+                if arg in defined_but_not_used:
+                    defined_but_not_used.remove(arg)
         if 'dest' in instr:
             defined_but_not_used.add(instr['dest'])
 
@@ -61,17 +61,26 @@ def remove_unused(instrs):
 
     instrs[:] = new_instrs 
 
-
-
-def tdce_block(block):
+def tdce_until_converge(instrs):
     """
-    Repeatedly runs TDCE pass on block until no instrunctions are removed
-    by the pass.
+    Peforms global trivial dead code elimination followed by local
+    trivial dead code elimination until the size of instrunctions
+    converges.
     """
-    while True:
-        deleted_inst = use_defs(block)
-        if not deleted_inst:
-            break
+    len1 = len(instrs)
+    remove_unused(instrs) 
+
+    blocks = list(form_blocks(instrs))
+    for block in blocks:
+        removed_overwritten_before_use(block) 
+    instrs = flatten(blocks)
+    len2 = len(instrs)
+
+    if len1 != len2:
+        return tdce_until_converge(instrs)
+    else:
+       return instrs 
+
 
 def tdce_prog(prog):
     """
@@ -80,12 +89,8 @@ def tdce_prog(prog):
     Then prints out the optimized program in JSON form.
     """
     for func in prog['functions']:
-        blocks = list(form_blocks(func['instrs']))
-        for block in blocks:
-            tdce_block(block)
-        instrs  = flatten(blocks)
-        remove_unused(instrs) # Globally removes variables never used
-        func['instrs'] = instrs
+        func['instrs'] = tdce_until_converge(func['instrs'])
+
     json.dump(prog, sys.stdout, indent=2, sort_keys=True)
 
 if __name__ == "__main__":
